46a7e5cbfeee76813450858b7572518ed95fadc6
Steve Kane <kanesteven@gmail.com>
2021-09-07 12:19:50 -0500

    made all access to data inside execute happen through internal methods to allow underlying data structure to change later

--- index.js
+++ index.js
@@ -23,55 +23,62 @@ class Chip8 {
   }
 
   execute(debugging=false) {
-    const l = this.memory[this.PC[0]]
-    const r = this.memory[this.PC[0] + 1]
-    
-    this.op[0] = highNibble(l)
-    this.op[1] = lowNibble(l)
-    this.op[2] = highNibble(r)
-    this.op[3] = lowNibble(r)
-
+    this.fetchOp()
     // 00E0 clear  
-    if (this.op[0] == 0b0000 && this.op[2] == 0b1110) {
+    if (this.getOp(0) == 0b0000 && this.getOp(2) == 0b1110) {
       this.display.fill(0)
       this.stepPC(INSTRUCTION_BYTE_LENGTH)
     } 
     // 1NNN jump. set PC to 12-bit value NNN
-    else if (this.op[0] == 0b0001) {
-      let nnn = bit12(this.op[1],this.op[2],this.op[3])
+    else if (this.getOp(0) == 0b0001) {
+      let nnn = bit12(this.getOp(1),this.getOp(2),this.getOp(3))
       this.setPC(nnn)
     }
     // 6XNN set VX to NN
-    else if (this.op[0] == 0b0110) {
-      let x = this.op[1]
-      let nn = bit8(this.op[2],this.op[3])
+    else if (this.getOp(0) == 0b0110) {
+      let x = this.getOp(1)
+      let nn = bit8(this.getOp(2),this.getOp(3))
       this.setRegister(x,nn)
       this.stepPC(INSTRUCTION_BYTE_LENGTH)
     }
     // 7XNN add NN to VX
-    else if (this.op[0] == 0b0111) {
-      let x = this.op[1]
-      let nn = bit8(this.op[2],this.op[3])
+    else if (this.getOp(0) == 0b0111) {
+      let x = this.getOp(1)
+      let nn = bit8(this.getOp(2),this.getOp(3))
       this.incrementRegister(x,nn)
       this.stepPC(INSTRUCTION_BYTE_LENGTH)
     }
     // ANNN set I to NNN
-    else if (this.op[0] == 0b1010) {
-      let nnn = bit12(this.op[1],this.op[2],this.op[3])
+    else if (this.getOp(0) == 0b1010) {
+      let nnn = bit12(this.getOp(1),this.getOp(2),this.getOp(3))
       this.setI(nnn)
       this.stepPC(INSTRUCTION_BYTE_LENGTH)
     }
     // DXYN draw N pixels tall sprite from memory[I] at X from VX and Y from VY
-    else if (this.op[0] == 0b1101) {
-      let x = this.V[this.op[1]] % SCREEN_WIDTH
-      let y = this.V[this.op[2]] % SCREEN_HEIGHT
-      let sprite = this.memory.slice(this.I, this.I+this.op[3])
-      this.drawSprite(x, y, this.op[3], sprite)
+    else if (this.getOp(0) == 0b1101) {
+      let xRegister = this.getOp(1)
+      let yRegister = this.getOp(2)
+      let x = this.getRegister(xRegister) % SCREEN_WIDTH
+      let y = this.getRegister(yRegister) % SCREEN_HEIGHT
+      let height = this.getOp(3)
+      let memoryOffset = this.getI()
+      this.drawSprite(x, y, height, memoryOffset)
       this.stepPC(INSTRUCTION_BYTE_LENGTH)
     }
     else {
       console.log(`Unrecognized instruction: ${this.op}`)
     }
+  }
+
+  fetchOp() {
+    this.op[0] = highNibble(this.memory[this.PC[0]])
+    this.op[1] = lowNibble(this.memory[this.PC[0]])
+    this.op[2] = highNibble(this.memory[this.PC[0] + 1])
+    this.op[3] = lowNibble(this.memory[this.PC[0] + 1])
+  }
+
+  getOp(nibbleIndex) {
+    return this.op[nibbleIndex]
   }
 
   stepPC(amt = INSTRUCTION_BYTE_LENGTH) {
@@ -82,8 +89,16 @@ class Chip8 {
     this.PC[0] = v
   }
 
+  getI() {
+    return this.I[0]
+  }
+
   setI(v) {
     this.I[0] = v
+  }
+
+  getRegister(i) {
+    return this.V[i]
   }
 
   setRegister(i,v) {
@@ -95,15 +110,15 @@ class Chip8 {
   }
 
   // TODO: handle the case of overlap/wrapping later
-  drawSprite(xMin,yMin,n,s) {
-    for (var j = 0; j < n; j++) {
+  drawSprite(xMin, yMin, height, memoryOffset) {
+    for (var j = 0; j < height; j++) {
       let y = yMin + j
 
       for (var i = 0; i < 8; i++) {
         let x = xMin + i
         let index = y * SCREEN_WIDTH + x
         let displayPixel = this.display[index]
-        let spritePixel = nthbit(7 - i, s[j])
+        let spritePixel = nthbit(7 - i, this.memory[memoryOffset + j])
 
         if (displayPixel > 0 && spritePixel > 0) {
           this.display[index] = 0
